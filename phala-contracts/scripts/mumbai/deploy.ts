7041e505c086af2e32687b93eb4c7049a5dc4eb3
0xshikhar <shikhar@iiitmanipur.ac.in>
2023-09-09 04:13:24 +0530

    phala oracle contract

---
+++ phala-contracts/scripts/mumbai/deploy.ts
@@ -0,0 +1,29 @@
+import { ethers } from "hardhat";
+import "dotenv/config";
+
+async function main() {
+  const TestLensApiConsumerContract = await ethers.getContractFactory("TestLensApiConsumerContract");
+
+  const [deployer] = await ethers.getSigners();
+
+  console.log('Deploying...');
+  console.log("deployer.address", deployer.address)
+  const attestor = process.env['MUMBAI_LENSAPI_ORACLE_ENDPOINT'] || deployer.address;  // When deploy for real e2e test, change it to the real attestor wallet.
+  console.log("attestor:", attestor)
+  const consumer = await TestLensApiConsumerContract.deploy(attestor);
+  await consumer.deployed();
+  console.log('Deployed', {
+    consumer: consumer.address,
+  });
+
+  console.log('Configuring...');
+  await consumer.connect(deployer).request("0x01c567");
+  console.log('Done');
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
